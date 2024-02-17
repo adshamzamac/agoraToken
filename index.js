06c3752e3fc3e78f35996c6affb03ab7b6771edf
Hamza-Ayed <masahamdev@gmail.com>
2024-02-17 17:06:28 +0300

    first commi4t

---
+++ index.js
@@ -0,0 +1,45 @@
+
+const express = require('express');
+const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
+
+const app = express();
+
+const port = 8080;
+const appId = '12994c6e707543e68d5638894d04f989';
+const appCertificate = 'e21a388f83034a159f2783889a6d7bcf';
+
+app.get('/token', (req, res) => {
+    // const channelName = req.query.channelName;
+    // const uid = req.query.uid || 0;
+    // const role = req.query.role || RtcRole.SUBSCRIBER;
+    // const expireTime = req.query.expireTime || 3600;
+
+    // const privilegeExpireTime = (Date.now() / 1000) + expireTime;
+    const channelName = req.query.channelName;
+    if (!channelName) {
+        return resp.status(500).json({ 'error': 'channelName is required' });
+    }
+    let uid = req.query.uid;
+    if (!uid) {
+        uid = 0;
+    }
+    let role = RtcRole.SUBSCRIBER;
+    if (req.query.role == 'publisher') {
+        role - RtcRole.PUBLISHER;
+    }
+    let expireTime = req.query.expireTime;
+    if (!expireTime || expireTime == '') {
+        expireTime = parseInt(expireTime, 10)
+    }
+    const currentTime = Math.floor(Date.now() / 1000);
+    const privilegeExpireTime = currentTime + expireTime;
+
+    const token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpireTime);
+
+    res.json({ token });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
